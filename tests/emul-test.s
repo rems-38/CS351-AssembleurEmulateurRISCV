e2cd7e8926d2419c68ca7a3f98eea58a9183b9f9
rems-38 <rem.mazzone@orange.fr>
2024-01-08 22:08:39 +0100

    fix bug in const.h [asm] (imm val)

--- tests/emul-test.s
+++ tests/emul-test.s
@@ -3,8 +3,13 @@ addi t2, a1, 72
 beq a1, t2, 2486
 sub s3, t2, a1
 add s3, t2, s3
-ld t0, 16(t6)
-sd a5, 1243(s10)
+#ld t0, 16(t6)
+#sd a5, 2486(s10)
 
+# EXPECTED
+# sp: 16384
+# a1: 20
+# t2: 92
+# s3: 164
 
 
